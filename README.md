afbbfd857847d28e6a760ac6367709e693f1d23e
Roman Lamsal <roman@lamsal.de>
2022-09-23 13:18:18 +0200

    add vue config

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # lamsal-node-defaults
+
+## Install
+
+Install `@rushstack/eslint-patch`, then copy this to your `.eslintrc.js` file.
+
+```js
+require("@rushstack/eslint-patch")
+
+module.exports = {
+    extends: "@lamsal-de"
+}
+```
---
+++ src/Foo.vue
@@ -0,0 +1,13 @@
+<template>
+    <div
+        data-maybe="or not?"
+        class="wowthisissuchalong string and i do not know if this is actually possible NOT to lint but hey"
+        id="maybe-this-id-gets-newlined"
+    ></div>
+</template>
+
+<script setup lang="ts">
+const foo = ""
+</script>
+
+<style scoped lang="scss"></style>

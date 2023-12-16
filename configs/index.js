5412c2fd05100cb4720fabda9738ae77c377e5bd
Roman Lamsal <roman@lamsal.de>
2023-12-16 21:55:24 +0100

    improvement: disable annoyences

--- configs/index.js
+++ configs/index.js
@@ -12,21 +12,16 @@ module.exports = {
     parserOptions: {
         parser: "@typescript-eslint/parser",
     },
-    overrides: [
-        {
-            files: ["**/*.tsx"],
-            plugins: ["react"],
-        },
-    ],
     rules: {
         "unused-imports/no-unused-imports": "error",
         curly: ["error", "all"],
-        "@typescript-eslint/no-non-null-assertion": "off",
-        "@typescript-eslint/explicit-module-boundary-types": "off",
-        "@typescript-eslint/no-unused-vars": "off",
-        "@typescript-eslint/ban-ts-comment": "off",
-        "@typescript-eslint/ban-types": "off",
-        "@typescript-eslint/no-empty-function": "off",
-        "@typescript-eslint/no-extra-semi": "off",
+        "@typescript-eslint/no-non-null-assertion": ["off"],
+        "@typescript-eslint/explicit-module-boundary-types": ["off"],
+        "@typescript-eslint/no-unused-vars": ["off"],
+        "@typescript-eslint/ban-ts-comment": ["off"],
+        "@typescript-eslint/ban-types": ["off"],
+        "@typescript-eslint/no-empty-function": ["off"],
+        "@typescript-eslint/no-extra-semi": ["off"],
+        "@typescript-eslint/no-empty-interface": ["off"],
     },
 }
